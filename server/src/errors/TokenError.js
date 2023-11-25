e7ff6f9cb62f43cbf896e0f33bdc77c6f902f555
victoria <vikabirukova22@gmail.com>
2023-11-25 13:08:03 +0200

    initial commit

---
+++ server/src/errors/TokenError.js
@@ -0,0 +1,9 @@
+const ApplicationError = require('./ApplicationError');
+
+class TokenError extends ApplicationError {
+  constructor (message) {
+    super(message || 'token error', 408);
+  }
+}
+
+module.exports = TokenError;
---
+++ server/src/jsonDB/models/Select.js
@@ -0,0 +1,24 @@
+const yup = require('yup');
+const Entity = require('../Entity');
+
+const selectScheme = yup.object().shape({
+  type: yup
+    .string()
+    .required()
+    .primaryKey(),
+  describe: yup
+    .string()
+    .required()
+    .primaryKey(),
+});
+
+module.exports = db =>
+  new Entity(
+    {
+      client: db.client,
+      modelName: 'Select',
+      tableName: 'Selects',
+      yupScheme: selectScheme,
+    },
+    db
+  );
